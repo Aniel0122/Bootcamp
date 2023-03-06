569e277f02295d147c98d6c9dda223bcf4571f04
Aniel_0122 <123281089+Aniel0122@users.noreply.github.com>
2023-03-06 13:33:14 -0400

    Add files via upload

---
+++ ejecicio3.py
@@ -0,0 +1,14 @@
+print ("------------Calcular la masa colpora-----------------")
+print ("                                                     ")
+print ("                                                     ")
+kg=0
+mt=0
+r=0
+kg= float(input("Cual es su peso en kg: "))
+mt= float(input("Cual es tu estatura en metros: "))
+
+r = kg / mt
+
+print("Su masa corporal es: " + str(r))
+
+input ("Precione entre para salir")
