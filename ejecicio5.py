7d6841faf9ed106ce8291adfc5874a710837100e
Aniel_0122 <123281089+Aniel0122@users.noreply.github.com>
2023-03-06 22:20:40 -0400

    Update ejecicio5.py

--- ejecicio5.py
+++ ejecicio5.py
@@ -1,16 +1,9 @@
-def es_bisiesto(anio):
-    """
-    Determina si un año es bisiesto o no.
-    
-    Args:
-    anio (int): El año a evaluar.
-    
-    Returns:
-    bool: True si el año es bisiesto, False en caso contrario.
-    """
-    if anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0):
-        return True
-    else:
-        return False
-print(es_bisiesto(2023))  # False
-print(es_bisiesto(2024))  # True
+def anyobisiesto():
+  anio: int = int(input("Introduce un año y para verificar si si es bisiesto:  "))
+
+  if(anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0)):
+     print("El año ha sido veridicada el,",anio, "es bisiesto")
+  else:
+      print("Ha sido verificado. El año",anio ,  "no es bisiesto")
+
+print(anyobisiesto())
